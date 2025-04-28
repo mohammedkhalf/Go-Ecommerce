9a2abb63a52a5a7c90158a985615da9ddf9bee58
Mohamed Khalf <mohamed.khalf@tamara.co>
2025-04-28 08:24:58 +0300

    add model structures and define controllers functions

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,1 +1,27 @@
 package controllers
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+func HashPassword(password string) string {
+}
+
+func VerifPassword(userPassword string, givenPassword string) (bool, error) {
+}
+
+func SignUp() gin.HandlerFunc {
+}
+
+func Login() gin.HandlerFunc {
+}
+
+func AddProduct() gin.HandlerFunc {
+}
+
+func ViewProduct() gin.HandlerFunc {
+}
+
+func SearchProduct() gin.HandlerFunc {
+}
--- routes/routes.go
+++ routes/routes.go
@@ -8,7 +8,7 @@ import (
 // general routes
 func UserRoutes(request *gin.Engine) {
 	request.POST("/user/signup", controllers.SignUp())
-	request.POST("/users/login", controllers.login())
+	request.POST("/users/login", controllers.Login())
 	request.POST("/admin/add-product", controllers.AddProduct())
 	request.GET("/users/product-view", controllers.ViewProduct())
 	request.POST("/users/search", controllers.SearchProduct())
